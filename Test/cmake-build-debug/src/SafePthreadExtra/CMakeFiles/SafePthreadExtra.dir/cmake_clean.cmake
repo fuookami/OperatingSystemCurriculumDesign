8513da63a78e96a00913658b734f17034a25b494
fuookami <fuookami@163.com>
2017-06-25 22:24:22 +0800

    搭建pc1的calculator和customer线程框架

---
+++ Test/cmake-build-debug/src/SafePthreadExtra/CMakeFiles/SafePthreadExtra.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SafePthreadExtra.dir/PthreadExtra.cpp.o"
+  "../../lib/libSafePthreadExtra.pdb"
+  "../../lib/libSafePthreadExtra.so"
+  "../../lib/libSafePthreadExtra.so.1.2"
+  "../../lib/libSafePthreadExtra.so.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SafePthreadExtra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Test/cmake-build-debug/src/SafePthreadExtra/CMakeFiles/SafePthreadExtra.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SafePthreadExtra.
+# This may be replaced when dependencies are built.
