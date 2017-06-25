dbaf4caa3fade3a7825a51e10dd6c2e48b7aaadd
fuookami <fuookami@163.com>
2017-06-25 12:42:24 +0800

    搭建pc1框架

--- Test/src/pc1/pc1.h
+++ Test/src/pc1/pc1.h
@@ -4,15 +4,33 @@
 
 #pragma once
 #include <iostream>
-#include <vector>
+#include <deque>
 
-class pc1
+class PC1Buff
 {
  public:
-  pc1(void);
-  ~pc1();
+  PC1Buff();
+  ~PC1Buff();
+
+  bool isEmpty(void) const;
+  bool isFull(void) const;
+  unsigned long size(void) const;
+
+  void push(const char ch);
+  char pop(void);
+
+ private:
+  static const unsigned long buffSize = 4;
+  std::deque<char> buffer;
+};
+
+class PC1
+{
+ public:
+  PC1(void);
+  ~PC1();
 
   void run();
  private:
-
+  PC1Buff buff1, buff2;
 };
