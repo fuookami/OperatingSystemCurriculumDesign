49da193d84c060b9b496fbfd38280831fd373160
fuookami <fuookami@163.com>
2017-06-26 22:51:36 +0800

    搭建pc2框架

--- Test/src/pc2/PC2.h
+++ Test/src/pc2/PC2.h
@@ -3,3 +3,32 @@
 //
 
 #pragma once
+
+#include "SafePthreadExtra.h"
+#include "PCBuffer.h"
+
+class PC2
+{
+ public:
+  ~PC2();
+
+  static PC2 &getReference();
+  void run();
+
+ private:
+  PC2();
+
+  static void *producer(void *args);
+  static void *calculator(void *args);
+  static void *customer(void *args);
+
+  void printMsg(const std::string &msg);
+
+ private:
+  pthread_t producerTid, calculatorTid, customerTid;
+  SPSafePthreadSema buff1MutexSema, buff2MutexSema, ioMutexSema;
+  SPSafePthreadSema buff1EmptySema, buff1FullSema;
+  SPSafePthreadSema buff2EmptySeam, buff2FullSema;
+  PCBuffer buff1, buff2;
+  volatile bool producerFinish, calculatorFinish;
+};
